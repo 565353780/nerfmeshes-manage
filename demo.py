aea18a3b4565038ce0ad6bae383ee399db108fd0
chLi <565353780@qq.com>
2022-09-28 21:56:24 +0800

    add two demo functions call script

---
+++ demo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from nerfmeshes_manage.Demo.train import demo as demo_train
+from nerfmeshes_manage.Demo.mesh import demo as demo_generate_mesh
+
+if __name__ == "__main__":
+    #  demo_train()
+    demo_generate_mesh()
