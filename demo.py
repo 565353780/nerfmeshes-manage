ef6a732201d04a7e9fdf81cd6173dae400ad5481
chLi <565353780@qq.com>
2022-09-29 16:40:36 +0800

    add one new demo

--- demo.py
+++ demo.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from nerfmeshes_manage.Demo.train import demo as demo_train
-from nerfmeshes_manage.Demo.mesh import demo as demo_generate_mesh
+from nerfmeshes_manage.Demo.mesh import demo as demo_nerf_mesh
+#  from nerfmeshes_manage.Demo.mesh_surface_ray import demo as demo_mesh_surface_ray
 
 if __name__ == "__main__":
     #  demo_train()
-    demo_generate_mesh()
+    demo_nerf_mesh()
+    #  demo_mesh_surface_ray()
